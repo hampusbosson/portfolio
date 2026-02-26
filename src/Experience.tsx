bc16a4fe73fafc9371606c2cb45c4fb615a3fb29
Hampus Bosson <hampus.bosson@outlook.com>
2026-02-26 20:10:51 +0100

    add post processing effects, vingette and noise

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,33 +1,41 @@
 import ProjectPage from "./screens/projects/ProjectPage";
-import { Perf } from "r3f-perf";
 import SetCamera from "./camera/setCamera";
 import type { Page } from "./types/types";
 import { Environment } from "@react-three/drei";
 import StartPage from "./screens/start/StartPage";
+import AboutMePage from "./screens/about-me/AboutMePage";
 import type React from "react";
+import { EffectComposer, Noise, Vignette } from "@react-three/postprocessing";
 
 interface ExperienceProps {
   activePage: Page;
   currentIndex: number;
   setCurrentIndex: React.Dispatch<React.SetStateAction<number>>;
+  onOpenProjectInfo: () => void;
 }
 
 export default function Experience({
   activePage,
   currentIndex,
   setCurrentIndex,
+  onOpenProjectInfo,
 }: ExperienceProps) {
   return (
     <>
+      <EffectComposer>
+        <Vignette offset={0.32} darkness={0.5} eskil={false} />
+        <Noise opacity={0.012} premultiply />
+      </EffectComposer>
       <color attach="background" args={["#191920"]} />
       <fog attach="fog" args={["#191920", 7, 15]} />
-      <SetCamera activeScreen={activePage} />
-      <Perf position="bottom-right" />
+      <SetCamera activeScreen={activePage} /> 
       <StartPage isActive={activePage === "start"} />
+      <AboutMePage isActive={activePage === "about"} />
       <ProjectPage
         currentIndex={currentIndex}
         setCurrentIndex={setCurrentIndex}
         isActive={activePage === "projects"}
+        onOpenProjectInfo={onOpenProjectInfo}
       />
       <Environment preset="city" background={false} />
     </>
